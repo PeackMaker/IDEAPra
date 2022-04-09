7a9af2e003fa0dbab41471c6583bbfd3aaa796de
peacemaker <peacemaker@gmail.com>
2022-04-09 16:57:52 +0800

    first Idea Commit

---
+++ src/main/java/com/win/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.win.git;
+
+/**
+ * @Desc:
+ * @Author: PEACEMAKER
+ * @Date: 2022/4/9
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Hello Git !!! ==》" +i);
+        }
+    }
+}
